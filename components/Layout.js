677c5fefc97bf1bef33329b694dbe6eb6d4b4c29
Işıl <isilozgu2016@gmail.com>
2024-12-18 01:20:32 +0300

    scrol to top when returning from dashboard to welcome page: somebody check this out

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import {useEffect} from "react";
 import Nav from "./Nav";
 import Landing from "./Landing";
 import Brands from "./Brands";
@@ -8,6 +9,11 @@ import Documents from "./Documents";
 import Footer from "./Footer";
 
 export default function Layout({navigate}) {
+  useEffect(() => {
+	// Scroll to top when the component is rendered
+	window.scrollTo(0, 0);
+  }, []); // Empty dependency array ensures this runs only when the component mounts
+
   return (
 
 
